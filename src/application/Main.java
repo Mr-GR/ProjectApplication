434f215d7ce83bef8d179eb1b28790c072a2f68a
Mr-GR <manny2144@icloud.com>
2023-12-08 14:10:06 -0500

    "Splash Screen and Login In Updated"

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,68 @@
 package application;
 	
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        showSplashScreen(primaryStage);
+    }
+
+    private void showSplashScreen(Stage primaryStage) {
+        // Create a separate stage for the splash screen
+        Stage splashStage = new Stage();
+        splashStage.initStyle(StageStyle.UNDECORATED);
+
+        try {
+           
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("SplashScreen.fxml"));
+            Parent splashRoot = loader.load();
+
+          
+            Scene splashScene = new Scene(splashRoot);
+            splashStage.setScene(splashScene);
+            splashStage.show();
+
+            SplashScreenController splashController = loader.getController();
+
+           
+            PauseTransition pause = new PauseTransition(Duration.seconds(3));
+            pause.setOnFinished(event -> {
+                splashStage.close();
+
+                loadMainContent(primaryStage);
+            });
+            pause.play();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+       
+    }
+
+    private void loadMainContent(Stage primaryStage) {
+        try {
+           
+            Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Rocket Red Air Login");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            
+        }
+    }
 }
---
+++ src/application/SplashScreenController.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.animation.PauseTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+
+public class SplashScreenController implements Initializable {
+
+	@FXML
+    private StackPane parent;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+		
+		private void showSplashScreen() {
+	        // Create a label with a loading message
+	        Label loadingLabel = new Label("Loading...");
+
+	        // Customize the appearance of the label as needed
+	        loadingLabel.setStyle("-fx-font-size: 24; -fx-text-fill: white;");
+
+	        // Add the label to the StackPane
+	        parent.getChildren().add(loadingLabel);
+
+	        // You can implement additional logic such as delays or animations here
+	        // For example, you can use PauseTransition for a delay
+	        PauseTransition pause = new PauseTransition();
+	        pause.setOnFinished(event -> {
+	            // Remove the label when the delay is over
+	            parent.getChildren().remove(loadingLabel);
+	        });
+	        pause.play();
+	    }
+
+		
+	
+	@FXML
+	private void close_app(MouseEvent event) {
+		System.exit(0);
+		
+	}
+	
+}	
